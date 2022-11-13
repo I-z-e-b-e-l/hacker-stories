5e222a4315fa110f82633113de994c7e01e4cd34
izebel <izebel@rocketmail.com>
2022-11-12 19:38:47 -0500

    react props in App.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,98 @@
 import * as React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    num_comments: 345,
-    objectID: 1
-  },
-  {
-    title: 'Redux',
-    url: 'httsp://redux.js.org',
-    num_comments: 233,
-    objectID: 2
-  }
-]
 
-function List(){
+
+function List(props){
   return (
     <div>
+      <h2>{props.title}</h2>
       <ul>
-        {list.map(function(item){
-          return(
-            <li key={item.objectID}>
-            {/* link does not work? */}
-              <a href='{item.url}'>{item.title}</a>
-              <p>Number of comments: {item.num_comments}</p>
-            </li>
-          );
+        {props.list.map(function(item){
+          return(<Item item={item} />);
         })}
       </ul>
     </div>
-  )
+  );
+}
+
+// a few different ways to write this in 1.3 "React Props"
+const Item = (props) => {
+  const item=props.item;
+
+  return (
+    <li key={item.objectID}>
+    {/* link does not work? */}
+      <a href='{item.url}'>{item.title}</a>
+      <p>Number of comments: {item.num_comments}</p>
+    </li>
+  );
 }
 
 function Search(){
+
+  // insert handler functions here:
+
+  const handleChange = (event) => {
+    // console.logs individual letters typed in
+    console.log(event.nativeEvent.data)
+    console.log(event)
+  }
+
+  const handleMouseOver = (event) => {
+    console.log(event)
+  }
+
   return(
     <div>
       <label htmlFor='search'>Search:</label>
-      <input id="search" type='text'/>
+      <input id="search" type='text' onChange={handleChange} onMouseOver={handleMouseOver}/>
     </div>
   )
 }
 
 
 function App() {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      num_comments: 345,
+      objectID: 1
+    },
+    {
+      title: 'Redux',
+      url: 'httsp://redux.js.org',
+      num_comments: 233,
+      objectID: 2
+    }
+  ];
+
+  const javascriptLibraries = [
+    {
+      title: 'JQuery',
+      url: 'https://jquery.org',
+      num_comments: 444,
+      objectID: 0
+    },
+    {
+      title: 'Angular',
+      url: 'httsp://angularjs.org',
+      num_comments: 233,
+      objectID: 4
+    }
+  ];
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
       
-      <List />
+      <List list={stories} title="React Ecosystem" />
+      <List list={javascriptLibraries} title="JS Libraries" />
       <Search />
-
+      <hr />
     </div>
   );
-}
+};
 
 export default App;
